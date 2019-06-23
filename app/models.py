fb98d1b2a637d6945e9eeff7cd7ea5b8daf49cff
JamesMutahi <mutahijames0@gmail.com>
2019-06-23 16:04:34 +0300

    set up classes

---
+++ app/models.py
@@ -0,0 +1,39 @@
+class Source :
+  '''
+  Source class to define Source Objects
+  '''
+  def __init__(self, id, name, description, url, category, language, country) :
+    self.id = id
+    self.name = name
+    self.description = description
+    self.url = url
+    self.category = category
+    self.language = language
+    self.country = country 
+
+class Top_Headlines :
+  '''
+  Source class to define Top_headlines Objects
+  '''
+  def __init__(self, author, title, description, url, urlToImage, publishedAt, content) :
+    self.author = author
+    self.title = title
+    self.description = description
+    self.url = url
+    self.urlToImage = urlToImage
+    self.publishedAt = publishedAt
+    self.content = content
+
+class Everything :
+  '''
+  Source class to define Everything Objects
+  '''
+  def __init__(self, author, title, description, url, urlToImage, publishedAt, content) :
+    self.author = author
+    self.title = title
+    self.description = description
+    self.url = url
+    self.urlToImage = urlToImage
+    self.publishedAt = publishedAt
+    self.content = content
+
